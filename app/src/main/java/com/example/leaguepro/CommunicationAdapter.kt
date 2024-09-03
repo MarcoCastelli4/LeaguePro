a04855e84f14a0742438ac29607d027c26f8dc98
Mattia <mattia.citroni00@gmail.com>
2024-09-03 13:46:16 +0200

    schermata dettagli match; controllo sull'inserimento di tutti i marcatori di un match; linea divisoria tra le righe delle statistiche e classifica

--- app/src/main/java/com/example/leaguepro/CommunicationAdapter.kt
+++ app/src/main/java/com/example/leaguepro/CommunicationAdapter.kt
@@ -5,7 +5,6 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.leaguepro.databinding.CardCommunicationsBinding
 
-
 class CommunicationAdapter(private val communications: List<Communication>) :
     RecyclerView.Adapter<CommunicationAdapter.CommunicationViewHolder>() {
 
--- app/src/main/java/com/example/leaguepro/StatisticsFragment.kt
+++ app/src/main/java/com/example/leaguepro/StatisticsFragment.kt
@@ -10,7 +10,6 @@ import android.view.ViewGroup
 import android.widget.TableLayout
 import android.widget.TableRow
 import android.widget.TextView
-import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
